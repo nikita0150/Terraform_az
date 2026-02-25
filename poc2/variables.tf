044c7fdc9960bf0e256189062fe91a6284ac94c7
NikitaGit09 <deshmukhnikita2001@gmail.com>
2026-02-26 00:15:57 +0530

    fix: add default values for resource group name and location variables

--- poc2/variables.tf
+++ poc2/variables.tf
@@ -1,9 +1,11 @@
 variable "resource_group_name" {
   type        = string
   description = "Name of resource group"
+  default = "my-rg"
 }
 
 variable "location" {
   type        = string
   description = "Azure region"
+  default = "eastus"
 }
